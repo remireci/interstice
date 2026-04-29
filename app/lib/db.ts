ed7adf6b65dd5d3904da961c3061af983bb6536d
reci.reciproque@gmail.com <reci.reciproque@gmail.com>
2026-04-29 11:08:24 +0200

    fix: unsubscribe page in node.js-2

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,47 +2,47 @@
 //
 // Production
 
-// import { Pool } from "pg";
-
-// const globalForDb = globalThis as unknown as {
-//   pool?: Pool;
-// };
-
-// export const pool =
-//   globalForDb.pool ??
-//   new Pool({
-//     connectionString: process.env.POSTGRES_URL ?? process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   });
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalForDb.pool = pool;
-// }
-
-// For dev
 import { Pool } from "pg";
 
 const globalForDb = globalThis as unknown as {
   pool?: Pool;
 };
 
-// export const pool =
-//   globalForDb.pool ??
-//   new Pool({
-//     connectionString: process.env.POSTGRES_URL,
-//   });
+export const pool =
+  globalForDb.pool ??
+  new Pool({
+    connectionString: process.env.POSTGRES_URL ?? process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
-// if (process.env.NODE_ENV !== "production") {
-//   globalForDb.pool = pool;
-// }
+if (process.env.NODE_ENV !== "production") {
+  globalForDb.pool = pool;
+}
 
-// dev only
-export const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL?.replace(/\?sslmode=.*$/, ""),
-  ssl:
-    process.env.NODE_ENV === "development"
-      ? { rejectUnauthorized: false }
-      : { ca: process.env.PG_CA_CERT, rejectUnauthorized: true },
-});
+// // For dev
+// import { Pool } from "pg";
+
+// const globalForDb = globalThis as unknown as {
+//   pool?: Pool;
+// };
+
+// // export const pool =
+// //   globalForDb.pool ??
+// //   new Pool({
+// //     connectionString: process.env.POSTGRES_URL,
+// //   });
+
+// // if (process.env.NODE_ENV !== "production") {
+// //   globalForDb.pool = pool;
+// // }
+
+// // dev only
+// export const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL?.replace(/\?sslmode=.*$/, ""),
+//   ssl:
+//     process.env.NODE_ENV === "development"
+//       ? { rejectUnauthorized: false }
+//       : { ca: process.env.PG_CA_CERT, rejectUnauthorized: true },
+// });
