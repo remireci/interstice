37af9fbc673ef3fadd8d95c1ea282379e25c92d8
reci.reciproque@gmail.com <reci.reciproque@gmail.com>
2026-04-30 12:26:47 +0200

    fix: sitemap 7

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.interstice.info/sitemaps/sitemap.xml",
+    sitemap: "https://www.interstice.info/sitemap.xml",
   };
 }
