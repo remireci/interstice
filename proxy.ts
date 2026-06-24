137dc3b97b5f4335519b227225360e61c5b8875a
reci.reciproque@gmail.com <reci.reciproque@gmail.com>
2026-06-24 10:14:44 +0200

    inactive

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const SITE_INACTIVE = false;
+const SITE_INACTIVE = true;
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
